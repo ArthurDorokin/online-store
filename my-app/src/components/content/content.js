31cc23e70843cc800665a74be7c73fbdf60404b0
Артур <a.u.dorokhin@gmail.com>
2020-01-27 20:50:01 +0200

    data_product json add

--- my-app/src/components/content/content.js
+++ my-app/src/components/content/content.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import './content.css';
 
-const Content = () => {
+let data_product = [
+        {
+            img: '../img/71epjBy+bNL._SX700_.jpg',
+            description: 'Pueraria Mirifica And Study Phyto Estrogens',
+            price: '$599.00',
+            id: 1
+        },
+        {
+            img: '../img/81kH6yCcnWL._SX700_.jpg',
+            description: 'Pueraria Mirifica And Study Phyto Estrogens',
+            price: '$325.00',
+            id: 2
+        },
+        {
+            img: '../img/81xFNXOeMfL._SX700_.jpg',
+            description: 'Pueraria Mirifica And Study Phyto Estrogens',
+            price: '$740.00',
+            id: 3
+        },
+        {
+            img: '../img/71epjBy+bNL._SX700_.jpg',
+            description: 'Pueraria Mirifica And Study Phyto Estrogens',
+            price: '$965.00',
+            id: 4
+        },
+        {
+            img: '../img/81kH6yCcnWL._SX700_.jpg',
+            description: 'Pueraria Mirifica And Study Phyto Estrogens',
+            price: '$965.00',
+            id: 5
+        },
+        {
+            img: '../img/81xFNXOeMfL._SX700_.jpg',
+            description: 'Pueraria Mirifica And Study Phyto Estrogens',
+            price: '$852.00',
+            id: 6
+        },
+    ];
 
+const elem = data_product.map((item) => {
+    return console.log(item);
+});
+
+const Content = () => {
     return (
         <div className="content">
             <div className="container">
