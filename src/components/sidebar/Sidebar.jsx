fdb6919d78bd90257acfd32a280a4bd4555b697e
Артур <a.u.dorokhin@gmail.com>
2020-02-26 22:23:52 +0200

    state open blocks

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './sidebar.css';
 import {sidebarList} from '../constants.js';
 
-const Sidebar = () => (
-    // test new branch
-    <div className="sidebar">
-        <div className="container">
-            <div className="sortFilter">
-                <div>FILTERS</div>
-                <div>RESET</div>
+class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false
+        };
+        this.toggleActive = this.toggleActive.bind(this);
+    }
+
+    toggleActive() {
+        this.setState({
+            active: !this.state.active
+        });
+    }
+
+    render() {
+        const activeClass = this.state.active ? "active" : "inactive";
+        return (
+            <div className="sidebar">
+                <div className="container">
+                    <div className="sortFilter">
+                        <div>FILTERS</div>
+                        <div>RESET</div>
+                    </div>
+                    <div className="list-section">
+                        <ul>
+                            {Object.entries(sidebarList).map(([key, items], index) => (
+                                <li key={index} className="list-item">
+                                    <h5 className={activeClass} onClick={this.toggleActive}>{key}</h5>
+                                    <div className="submenu">
+                                        <ul>
+                                            {items.map((item, jIndex) => <li key={jIndex}>{item}</li>)}
+                                        </ul>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div className="list-section">
-                <ul>
-                    {Object.entries(sidebarList).map(([key, items], index) => (
-                        <li key={index} className="list-item">
-                            <h5>{key}</h5>
-                            <div className="submenu">
-                                <ul>
-                                    {items.map((item, jIndex) => <li key={jIndex}>{item}</li>)}
-                                </ul>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    </div>
-);
+        )
+    }
+}
 
 export default Sidebar
