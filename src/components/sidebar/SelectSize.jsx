fa40d8ab59f6ff642ca4447e3d5a55852b886c75
Артур <a.u.dorokhin@gmail.com>
2020-03-02 22:14:11 +0200

    selectSize

--- src/components/sidebar/SelectSize.jsx
+++ src/components/sidebar/SelectSize.jsx
@@ -1,9 +1,18 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 
 export const SelectSize = ({selectSize}) => {
+    const [active, setActive] = useState({
+        class: "active",
+        id: "0"
+    });
+
+    const toggleActive = () => {
+        const sdf = active.id === 0;
+    };
+    console.log(active);
     return (
         <div className="wrapSelectSize">
-            {selectSize.sizes.map((item, index) => <div className="size-item" key={item.id}>{item.size}</div>)}
+            {selectSize.sizes.map((item, index) => <div  id={item.id} className={`${"size-item"} ${active}`} onClick={toggleActive} key={index}>{item.size}</div>)}
         </div>
     )
 };
